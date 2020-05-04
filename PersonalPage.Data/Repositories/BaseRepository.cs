77825de80dcde93069f470aee36a39413dae8318
alexsteeel <suchkov.as.1989@gmail.com>
2020-05-04 19:26:57 +0300

    Add one base repository.

---
+++ PersonalPage.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalPage.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalPage.Data.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        internal readonly ApplicationDbContext context;
+
+        public BaseRepository(ApplicationDbContext appContext)
+        {
+            this.context = appContext;
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            context.Add<T>(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> FindAsync(Expression<Func<T, bool>> filter, string includeProperties = null)
+        {
+            IQueryable<T> query = context.Set<T>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await context.FindAsync<T>(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAsync(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null)
+        {
+            IQueryable<T> query = context.Set<T>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+    }
+}
---
+++ PersonalPage.Data/Repositories/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PersonalPage.Data.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+
+        Task<T> FindAsync(Expression<Func<T, bool>> filter,
+            string includeProperties = null);
+
+        Task<T> GetAsync(int id);
+
+        Task<IEnumerable<T>> GetAsync(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null);
+    }
+}
