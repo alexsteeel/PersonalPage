32a71a1ddc9aa1d5fc41317d9f446c16dfd5e8e1
alexsteeel <suchkov.as.1989@gmail.com>
2020-05-06 07:35:04 +0300

    Add project PersonalPage.WebApi.UnitTests.

---
+++ PersonalPage.WebApi.UnitTests/AccountsControllerUnitTests.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using PersonalPage.Core;
+using PersonalPage.WebApi.Controllers;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PersonalPage.WebApi.UnitTests
+{
+    [TestFixture]
+    public class AccountsControllerUnitTests
+    {
+        [Test]
+        public async Task Post_UseCaseSucceeds_ReturnsOk()
+        {
+            // arrange
+            var mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository
+                .Setup(repo => repo.Create(It.IsAny<User>(), It.IsAny<string>()))
+                .Returns(Task.FromResult(new CreateUserResponse("", true)));
+
+            // fakes
+            var outputPort = new RegisterUserPresenter();
+            var useCase = new RegisterUserUseCase(mockUserRepository.Object);
+
+            var controller = new AccountsController(useCase, outputPort);
+
+            // act
+            var result = await controller.Post(new Models.Request.RegisterUserRequest());
+
+            // assert
+            var statusCode = ((ContentResult)result).StatusCode;
+            Assert.True(statusCode.HasValue && statusCode.Value == (int)HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Post_ModelValidationFails_ReturnsBadRequest()
+        {
+            // arrange
+            var controller = new AccountsController(null, null);
+            controller.ModelState.AddModelError("FirstName", "Required");
+
+            // act
+            var result = await controller.Post(null);
+
+            // assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+            Assert.IsInstanceOf<SerializableError>((result as BadRequestObjectResult).Value);
+        }
+    }
+}
