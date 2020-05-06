c7dad5ee5dd222c6c8f2c3840bd57659d2d088de
alexsteeel <suchkov.as.1989@gmail.com>
2020-05-06 07:23:23 +0300

    Add fluent validation for web api and validator for RegisterUserRequest.

---
+++ PersonalPage.WebApi/Models/Validation/RegisterUserRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PersonalPage.WebApi.Models.Request;
+
+namespace PersonalPage.WebApi.Models.Validation
+{
+    public class RegisterUserRequestValidator : AbstractValidator<RegisterUserRequest>
+    {
+        public RegisterUserRequestValidator()
+        {
+            RuleFor(x => x.UserName).Length(5, 255);
+            RuleFor(x => x.Password).Length(6, 15);
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password);
+        }
+    }
+}
